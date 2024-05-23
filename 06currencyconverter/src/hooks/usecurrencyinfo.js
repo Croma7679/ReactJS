b049c037541e7d19d2f422fe48a55e670d98003f
Aashish <aashishgole999@gmail.com>
2024-05-23 17:45:59 +0530

    video11

---
+++ 06currencyconverter/src/hooks/usecurrencyinfo.js
@@ -0,0 +1,8 @@
+import {useEffect, useState} from "react"
+
+function useCurrencyInfo(currency){
+    const [data,setdata]=useState({})
+    useEffect{()=>{
+        fetch( `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/${currencies}/usd.json`).then((res)=>res.json())
+    },[]}
+}
--- 07passwordgenerate/src/App.jsx
+++ 07passwordgenerate/src/App.jsx
@@ -1,13 +1,93 @@
-import { useState } from 'react'
+import { useState, useCallback ,useEffect,useRef} from 'react'
 
 import './App.css'
 
 function App() {
   const [length, setlength] = useState(8)
+  const[Numallowed, setNumallowed]=useState(false)
+  const[charallowed,setcharallowed]=useState(false)
+  const[password,setpassword]=useState("")
+
+  const passref=useRef(null)
+ 
+  const passwordGenerator= useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(Numallowed){
+      str+="0123456789"
+    }
+    if(charallowed){
+      str+="!@#$%^&*()"
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char=Math.floor(Math.random()*str.length+1)
+      pass+=str.charAt(char)
+      
+    }
+    setpassword(pass)
+
+  },[length,Numallowed,charallowed,setpassword]) 
+
+  const copyPasswordtoClip=useCallback(()=>{
+    passref.current?.select()
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,Numallowed,charallowed,passwordGenerator])
+
 
   return (
     <>
       <h1 className='text-4xl text-center'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500'>Password Generator
+         <div className='className="flex shadow rounded-lg overflow-hidden mb-4"'>
+          <input type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='password'
+          readOnly
+          ref={passref}/>
+          <button
+          onClick={copyPasswordtoClip}>copy</button>
+         </div>
+         <div className='flex text-sm gap-x-2'>
+          <div className='flex-iterms-center gap-x-1'>
+            <input type="range"
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e)=>{setlength(e.target.value)}} />
+            <label>Length:{length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+            type="checkbox"
+            defaultChecked={Numallowed}
+            id="numberInput"
+            onChange={()=>{
+              setNumallowed((prev)=>!prev)
+            }}
+            />
+            <label htmlFor='numberInput'>Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+            type="checkbox"
+            defaultChecked={charallowed}
+            id="charInput"
+            onChange={()=>{
+              setcharallowed((prev)=>!prev)
+            }}
+            />
+            <label htmlFor='charInput'>Characters</label>
+          </div>
+         </div>
+      </div>
     </>
   )
 }
